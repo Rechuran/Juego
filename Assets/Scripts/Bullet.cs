776809e4c06c405f8fa7ae5db61395f95250b139
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2023-05-11 22:08:55 -0400

    arma y bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    public float speed = 3;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position+transform.right*speed*Time.fixedDeltaTime);
+    }
+}
